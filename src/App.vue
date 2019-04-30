ea14f4710ebf819dccc8ba9080f48286cfe269dd
KansasMyers <kansasmyers360@gmail.com>
2019-04-30 16:45:37 -0300

    Primeiro Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Paginator :list="itens" :rows="6" :fields="['id', 'nome']" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Paginator from './components/Paginator.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Paginator
+  },
+  data () {
+    return {
+      itens: [
+        { id: 1, nome: 'José Victor' },
+        { id: 2, nome: 'Nicholas Victor' },
+        { id: 3, nome: 'Dubs Victor' },
+        { id: 4, nome: 'Scrach Victor' },
+        { id: 5, nome: 'Devil Victor' },
+        { id: 6, nome: 'José Victor' },
+        { id: 7, nome: 'Nicholas Victor' },
+        { id: 8, nome: 'Dubs Victor' },
+        { id: 9, nome: 'Scrach Victor' },
+        { id: 10, nome: 'Devil Victor' },
+        { id: 11, nome: 'José Victor' },
+        { id: 12, nome: 'Nicholas Victor' },
+        { id: 13, nome: 'Dubs Victor' },
+        { id: 14, nome: 'Scrach Victor' },
+        { id: 15, nome: 'Devil Victor' },
+        { id: 16, nome: 'José Victor' },
+        { id: 17, nome: 'Nicholas Victor' },
+        { id: 18, nome: 'Dubs Victor' },
+        { id: 19, nome: 'Scrach Victor' },
+        { id: 20, nome: 'Devil Victor' }
+      ]
+    }
   }
 }
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Ubuntu");
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Ubuntu", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Paginator.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="paginator">
+    <ul>
+      <li v-for="(p, index) in pageData" :key="index">
+        {{ renderFields(p) }}
+      </li>
+    </ul>
+    <button :disabled="page == 0" @click="previousPage">Previous</button>
+    <button :disabled="page >= pageCount -1" @click="nextPage">Next</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Paginator',
+  data () {
+    return {
+      page: 0
+    }
+  },
+  props: {
+    list: {
+      type: Array,
+      required: true
+    },
+    fields: {
+      type: Array,
+      required: true
+    },
+    rows: {
+      type: Number,
+      required: false,
+      default: 5
+    }
+  },
+  methods: {
+    previousPage () {
+      this.page--
+    },
+
+    nextPage () {
+      this.page++
+    },
+
+    renderFields (obj) {
+      let i = 0
+      let renderText = ''
+
+      this.fields.forEach(field => {
+        if (i !== 0) { renderText += ' - ' }
+
+        renderText += field.toUpperCase() + ': ' + this._.get(obj, field, 'Invalid Field')
+
+        i++
+      })
+
+      return renderText
+    }
+  },
+  computed: {
+    pageCount () {
+      let l = this.list.length
+      let s = this.rows
+
+      return Math.ceil(l / s)
+    },
+
+    pageData () {
+      const start = this.page * this.rows
+      const end = start + this.rows
+
+      return this.list.slice(start, end)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
